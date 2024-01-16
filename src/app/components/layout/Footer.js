d4ca345b393ab81f547bdbb73fffe60974ec2a51
skllzrmy.tez <joe@poundfit.com>
2024-01-15 20:33:34 -0800

    🎨 style(Footer.js): reformat code for better readability and maintainability 🔧 chore(Footer.js): add a new contributor to the footer 🔧 chore(Footer.js): update links in the footer to point to the correct repositories

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -4,46 +4,39 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Made with 💙 by "}
-      <Link color="inherit" href="https://twitter.com/KStasii/" target="_blank">
-        KStasi
-      </Link>
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {"Made with 💙 by "}
+            <Link color="inherit" href="https://twitter.com/KStasii/" target="_blank">
+                KStasi
+            </Link>
+            {"Modified with ♥️ by "}
+            <Link color="inherit" href="https://twitter.com/skllzarmy/" target="_blank">
+                skllzrmy.tez
+            </Link>
+        </Typography>
+    );
 }
 
 function Reference({ title, link }) {
-  return (
-    <Typography
-      variant="subtitle1"
-      align="center"
-      color="text.secondary"
-      component="p"
-    >
-      <Link color="inherit" href={link} target="_blank">
-        {title}
-      </Link>
-    </Typography>
-  );
+    return (
+        <Typography variant="subtitle1" align="center" color="text.secondary" component="p">
+            <Link color="inherit" href={link} target="_blank">
+                {title}
+            </Link>
+        </Typography>
+    );
 }
 function Footer() {
-  return (
-    <Box component="footer" sx={{ py: 5 }}>
-      <Container maxWidth="lg">
-        <Reference
-          title="Smart Contracts"
-          link="https://github.com/oxheadalpha/smart-contracts"
-        ></Reference>
-        <Reference
-          title="Frontend"
-          link="https://github.com/KStasi/fa2-deployer"
-        ></Reference>
-        <Copyright />
-      </Container>
-    </Box>
-  );
+    return (
+        <Box component="footer" sx={{ py: 5 }}>
+            <Container maxWidth="lg">
+                <Reference title="Smart Contracts" link="https://github.com/oxheadalpha/smart-contracts"></Reference>
+                <Reference title="Frontend" link="https://github.com/KStasi/fa2-deployer"></Reference>
+                <Copyright />
+            </Container>
+        </Box>
+    );
 }
 
 export default Footer;
