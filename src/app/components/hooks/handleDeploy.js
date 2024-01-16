7e601e37a6999d4585632ce82c236bdfed0b3aa4
skllzrmy.tez <joe@poundfit.com>
2024-01-15 16:36:11 -0800

    fixing eqeq to eqeqeq

--- src/app/components/hooks/handleDeploy.js
+++ src/app/components/hooks/handleDeploy.js
@@ -4,76 +4,68 @@ import fa2MultiAsset from "../assets/MultiAsset.json";
 import fa2GranularMultiAsset from "../assets/GranularMultiAsset.json";
 import { BigNumber } from "bignumber.js";
 
-const handleDeploy = async (
-  admin,
-  contractName,
-  contractDescription,
-  tokensString,
-  type,
-  wallet,
-  setFetching
-) => {
-  setFetching(true);
-  try {
-    const tokens = JSON.parse(tokensString);
-    let metadata = MichelsonMap.fromLiteral({
-      "": Buffer("tezos-storage:contents", "ascii").toString("hex"),
-      contents: Buffer(
-        JSON.stringify({
-          version: "v0.0.1",
-          name: contractName,
-          description: contractDescription,
-          authors: ["FA2 Bakery"],
-          source: {
-            tools: ["Ligo"],
-          },
-          interfaces: ["TZIP-012", "TZIP-016"],
-        }),
-        "ascii"
-      ).toString("hex"),
-    });
-    const storage = {
-      admin: {
-        admin: admin,
-        pending_admin: null,
-        paused: type == "Basic" ? false : MichelsonMap.fromLiteral({}),
-      },
-      assets: {
-        token_total_supply: MichelsonMap.fromLiteral({}),
-        ledger: MichelsonMap.fromLiteral({}),
-        operators: MichelsonMap.fromLiteral({}),
-        token_metadata: MichelsonMap.fromLiteral({}),
-      },
-      metadata: metadata,
-    };
-    tokens.forEach((token, index) => {
-      const tokenSupply = token.supply * new BigNumber(10).pow(token.decimals);
-      storage.assets.token_total_supply.set(index, tokenSupply);
-      storage.assets.ledger.set([admin, index], tokenSupply);
-      storage.assets.token_metadata.set(index, {
-        token_id: index,
-        token_info: MichelsonMap.fromLiteral({
-          symbol: Buffer(token.symbol, "ascii").toString("hex"),
-          name: Buffer(token.name, "ascii").toString("hex"),
-          decimals: Buffer(token.decimals, "ascii").toString("hex"),
-          shouldPreferSymbol: Buffer("true", "ascii").toString("hex"),
-          description: Buffer(token.description, "ascii").toString("hex"),
-          thumbnailUri: Buffer(token.icon, "ascii").toString("hex"),
-        }),
-      });
-    });
-    console.log(fa2MultiAsset);
-    await wallet
-      .originate({
-        code: type == "Basic" ? fa2MultiAsset : fa2GranularMultiAsset,
-        storage,
-      })
-      .send();
-  } catch (e) {
-    console.error(e);
-  } finally {
-    setFetching(false);
-  }
+const handleDeploy = async (admin, contractName, contractDescription, tokensString, type, wallet, setFetching) => {
+    setFetching(true);
+    try {
+        const tokens = JSON.parse(tokensString);
+        let metadata = MichelsonMap.fromLiteral({
+            "": Buffer("tezos-storage:contents", "ascii").toString("hex"),
+            contents: Buffer(
+                JSON.stringify({
+                    version: "v0.0.1",
+                    name: contractName,
+                    description: contractDescription,
+                    authors: ["FA2 Bakery"],
+                    source: {
+                        tools: ["Ligo"],
+                    },
+                    interfaces: ["TZIP-012", "TZIP-016"],
+                }),
+                "ascii"
+            ).toString("hex"),
+        });
+        const storage = {
+            admin: {
+                admin: admin,
+                pending_admin: null,
+                paused: type === "Basic" ? false : MichelsonMap.fromLiteral({}),
+            },
+            assets: {
+                token_total_supply: MichelsonMap.fromLiteral({}),
+                ledger: MichelsonMap.fromLiteral({}),
+                operators: MichelsonMap.fromLiteral({}),
+                token_metadata: MichelsonMap.fromLiteral({}),
+            },
+            metadata: metadata,
+        };
+        tokens.forEach((token, index) => {
+            const tokenSupply = token.supply * new BigNumber(10).pow(token.decimals);
+            storage.assets.token_total_supply.set(index, tokenSupply);
+            storage.assets.ledger.set([admin, index], tokenSupply);
+            storage.assets.token_metadata.set(index, {
+                token_id: index,
+                token_info: MichelsonMap.fromLiteral({
+                    symbol: Buffer(token.symbol, "ascii").toString("hex"),
+                    name: Buffer(token.name, "ascii").toString("hex"),
+                    decimals: Buffer(token.decimals, "ascii").toString("hex"),
+                    shouldPreferSymbol: Buffer("true", "ascii").toString("hex"),
+                    description: Buffer(token.description, "ascii").toString("hex"),
+                    thumbnailUri: Buffer(token.icon, "ascii").toString("hex"),
+                }),
+            });
+        });
+        console.log(fa2MultiAsset);
+        await wallet
+            .originate({
+                code: type === "Basic" ? fa2MultiAsset : fa2GranularMultiAsset,
+                storage,
+            })
+            .send();
+    } catch (e) {
+        console.error(e);
+    } finally {
+        setFetching(false);
+    }
 };
 
 export default handleDeploy;
--- src/app/components/layout/FormBox.js
+++ src/app/components/layout/FormBox.js
@@ -9,198 +9,173 @@ import MainSelect from "../atoms/MainSelect";
 import HorizontalStack from "../atoms/HorizontalStack";
 import Stack from "@mui/material/Stack";
 
-const validateInput = (
-  admin,
-  contractName,
-  contractDescription,
-  tokensString
-) => {
-  if (
-    [admin, contractName, contractDescription].some(
-      (str) => !str || str.trim() == ""
+const validateInput = (admin, contractName, contractDescription, tokensString) => {
+    if ([admin, contractName, contractDescription].some((str) => !str || str.trim() === "")) return false;
+    if (
+        tokensString.some((token) => {
+            return Object.values(token).some((str) => !str || str.trim() === "");
+        })
     )
-  )
-    return false;
-  if (
-    tokensString.some((token) => {
-      return Object.values(token).some((str) => !str || str.trim() == "");
-    })
-  )
-    return false;
-  return true;
+        return false;
+    return true;
 };
 const FormBox = () => {
-  const { pkh, Tezos } = useBeacon();
+    const { pkh, Tezos } = useBeacon();
 
-  const [tokensString, setTokens] = useState("[]");
-  const tokens = JSON.parse(tokensString);
-  const [admin, setAdmin] = useState(pkh);
-  const [contractName, setContractName] = useState("");
-  const [contractDescription, setContractDescription] = useState("");
-  const [tokenType, setTokenType] = useState("Basic");
-  const [, setFetching] = useState(false);
+    const [tokensString, setTokens] = useState("[]");
+    const tokens = JSON.parse(tokensString);
+    const [admin, setAdmin] = useState(pkh);
+    const [contractName, setContractName] = useState("");
+    const [contractDescription, setContractDescription] = useState("");
+    const [tokenType, setTokenType] = useState("Basic");
+    const [, setFetching] = useState(false);
 
-  const handleClick = useCallback(async () => {
-    if (validateInput(admin, contractName, contractDescription, tokens)) {
-      await handleDeploy(
-        admin,
-        contractName,
-        contractDescription,
-        tokensString,
-        tokenType,
-        Tezos.wallet,
-        setFetching
-      );
-    }
-  }, [
-    setFetching,
-    Tezos.wallet,
-    tokensString,
-    admin,
-    contractName,
-    contractDescription,
-    tokenType,
-    tokens,
-  ]);
-  return (
-    <Paper
-      component="form"
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-      sx={{
-        px: 8,
-        py: 5,
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Stack direction="column" alignItems="center" spacing={2}>
-        <FormField
-          label="Admin"
-          onChange={(e) => setAdmin(e.target.value)}
-        ></FormField>
-        <FormField
-          label="Contract name"
-          onChange={(e) => setContractName(e.target.value)}
-        ></FormField>
-        <FormField
-          label="Contract description"
-          multiline="true"
-          onChange={(e) => setContractDescription(e.target.value)}
-          maxRows="4"
-          minRows="4"
-        ></FormField>
-        <HorizontalStack>
-          <MainSelect
-            text="Supply Type"
-            defaultValue={tokenType}
-            onChange={(e) => setTokenType(e.target.value)}
-            items={["Basic", "Granular"]}
-          ></MainSelect>
-          <MainButton
-            colorType="type3"
-            text="Add Asset"
-            onClick={() =>
-              setTokens(
-                JSON.stringify(
-                  tokens.concat({
-                    name: "",
-                    symbol: "",
-                    decimals: "",
-                    supply: "",
-                    description: "",
-                    icon: "",
-                  })
-                )
-              )
-            }
-          ></MainButton>{" "}
-        </HorizontalStack>
-        {tokens.map((tokenInfo, index) => (
-          <React.Fragment key={index}>
-            <HorizontalStack>
-              <Box
-                sx={{
-                  width: 200,
-                  font: "normal normal bold 16px/22px Roboto",
-                }}
-              >
-                Asset {index}
-              </Box>
-              <MainButton
-                colorType="type3"
-                text="Remove Asset"
-                onClick={() => {
-                  tokens.splice(index, 1);
-                  setTokens(JSON.stringify(tokens));
-                }}
-              ></MainButton>{" "}
-            </HorizontalStack>
-            <HorizontalStack>
-              <FormField
-                label="Name"
-                onChange={(e) => {
-                  tokens[index].name = e.target.value;
-                  setTokens(JSON.stringify(tokens));
-                }}
-              ></FormField>
-              <FormField
-                label="Symbol"
-                onChange={(e) => {
-                  tokens[index].symbol = e.target.value;
-                  setTokens(JSON.stringify(tokens));
-                }}
-              ></FormField>
-            </HorizontalStack>
-            <HorizontalStack>
-              <FormField
-                label="Supply"
-                type="number"
-                onChange={(e) => {
-                  tokens[index].supply = e.target.value;
-                  setTokens(JSON.stringify(tokens));
-                }}
-              ></FormField>
-              <FormField
-                label="Decimals"
-                type="number"
-                onChange={(e) => {
-                  tokens[index].decimals = e.target.value;
-                  setTokens(JSON.stringify(tokens));
-                }}
-              ></FormField>
-            </HorizontalStack>
-            <FormField
-              label="Icon"
-              type="text"
-              onChange={(e) => {
-                tokens[index].icon = e.target.value;
-                setTokens(JSON.stringify(tokens));
-              }}
-            ></FormField>
-            <FormField
-              label="Description"
-              multiline="true"
-              maxRows="4"
-              minRows="4"
-              onChange={(e) => {
-                tokens[index].description = e.target.value;
-                setTokens(JSON.stringify(tokens));
-              }}
-            ></FormField>
-          </React.Fragment>
-        ))}
-        <MainButton
-          type="submit"
-          colorType="type2"
-          text="Deploy"
-          onClick={handleClick}
-          disabled={!pkh}
-        ></MainButton>
-      </Stack>
-    </Paper>
-  );
+    const handleClick = useCallback(async () => {
+        if (validateInput(admin, contractName, contractDescription, tokens)) {
+            await handleDeploy(
+                admin,
+                contractName,
+                contractDescription,
+                tokensString,
+                tokenType,
+                Tezos.wallet,
+                setFetching
+            );
+        }
+    }, [setFetching, Tezos.wallet, tokensString, admin, contractName, contractDescription, tokenType, tokens]);
+    return (
+        <Paper
+            component="form"
+            onSubmit={(event) => {
+                event.preventDefault();
+            }}
+            sx={{
+                px: 8,
+                py: 5,
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <Stack direction="column" alignItems="center" spacing={2}>
+                <FormField label="Admin" onChange={(e) => setAdmin(e.target.value)}></FormField>
+                <FormField label="Contract name" onChange={(e) => setContractName(e.target.value)}></FormField>
+                <FormField
+                    label="Contract description"
+                    multiline="true"
+                    onChange={(e) => setContractDescription(e.target.value)}
+                    maxRows="4"
+                    minRows="4"
+                ></FormField>
+                <HorizontalStack>
+                    <MainSelect
+                        text="Supply Type"
+                        defaultValue={tokenType}
+                        onChange={(e) => setTokenType(e.target.value)}
+                        items={["Basic", "Granular"]}
+                    ></MainSelect>
+                    <MainButton
+                        colorType="type3"
+                        text="Add Asset"
+                        onClick={() =>
+                            setTokens(
+                                JSON.stringify(
+                                    tokens.concat({
+                                        name: "",
+                                        symbol: "",
+                                        decimals: "",
+                                        supply: "",
+                                        description: "",
+                                        icon: "",
+                                    })
+                                )
+                            )
+                        }
+                    ></MainButton>{" "}
+                </HorizontalStack>
+                {tokens.map((tokenInfo, index) => (
+                    <React.Fragment key={index}>
+                        <HorizontalStack>
+                            <Box
+                                sx={{
+                                    width: 200,
+                                    font: "normal normal bold 16px/22px Roboto",
+                                }}
+                            >
+                                Asset {index}
+                            </Box>
+                            <MainButton
+                                colorType="type3"
+                                text="Remove Asset"
+                                onClick={() => {
+                                    tokens.splice(index, 1);
+                                    setTokens(JSON.stringify(tokens));
+                                }}
+                            ></MainButton>{" "}
+                        </HorizontalStack>
+                        <HorizontalStack>
+                            <FormField
+                                label="Name"
+                                onChange={(e) => {
+                                    tokens[index].name = e.target.value;
+                                    setTokens(JSON.stringify(tokens));
+                                }}
+                            ></FormField>
+                            <FormField
+                                label="Symbol"
+                                onChange={(e) => {
+                                    tokens[index].symbol = e.target.value;
+                                    setTokens(JSON.stringify(tokens));
+                                }}
+                            ></FormField>
+                        </HorizontalStack>
+                        <HorizontalStack>
+                            <FormField
+                                label="Supply"
+                                type="number"
+                                onChange={(e) => {
+                                    tokens[index].supply = e.target.value;
+                                    setTokens(JSON.stringify(tokens));
+                                }}
+                            ></FormField>
+                            <FormField
+                                label="Decimals"
+                                type="number"
+                                onChange={(e) => {
+                                    tokens[index].decimals = e.target.value;
+                                    setTokens(JSON.stringify(tokens));
+                                }}
+                            ></FormField>
+                        </HorizontalStack>
+                        <FormField
+                            label="Icon"
+                            type="text"
+                            onChange={(e) => {
+                                tokens[index].icon = e.target.value;
+                                setTokens(JSON.stringify(tokens));
+                            }}
+                        ></FormField>
+                        <FormField
+                            label="Description"
+                            multiline="true"
+                            maxRows="4"
+                            minRows="4"
+                            onChange={(e) => {
+                                tokens[index].description = e.target.value;
+                                setTokens(JSON.stringify(tokens));
+                            }}
+                        ></FormField>
+                    </React.Fragment>
+                ))}
+                <MainButton
+                    type="submit"
+                    colorType="type2"
+                    text="Deploy"
+                    onClick={handleClick}
+                    disabled={!pkh}
+                ></MainButton>
+            </Stack>
+        </Paper>
+    );
 };
 
 export default FormBox;
